c0e7a1b79560068c716b65f0195679397fe32776
Samar-Memon <samar56786raza@gmail.com>
2024-03-09 22:38:11 +0500

    Js-Assignment-Var4Num

---
+++ varfornum.js
@@ -0,0 +1,11 @@
+var age = 15;
+var site = 14;
+var items = 50;
+alert('I am ' + age + ' Years Old');
+document.write('I am ' + age + ' Years Old.' + '<br>' + '<br>');
+
+alert('You Have Visited this Site ' + site + ' times.');
+document.write('You Have Visited this Site ' + site + ' times.' + '<br>' + '<br>');
+
+alert('John Order ' + items + ' Lays Chips.');
+document.write('John Order ' + items + ' Lays Chips.' + '<br>' + '<br>');
